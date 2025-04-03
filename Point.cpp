e3b1a86399aba99ede3a5e184eec123b9e35d674
Marcoosbw <marcos.bringaswu15@bcmail.cuny.edu>
2025-04-03 06:55:33 +0000

    Submission

--- Point.cpp
+++ Point.cpp
@@ -3,3 +3,21 @@
 #include "Point.h"
 
 using namespace std;
+
+    // Constructor
+    Point(double xCoord, double yCoord) {
+        x = xCoord;
+        y = yCoord;
+    }
+
+    // Member function to calculate distance to another Point
+    double distanceTo(const Point& other) const {
+        // int powX = pow(other.x - this.x);
+        // int powY = pow(other.y - thix.y);
+        return sqrt( pow(other.x - this.x), pow(other.y - thix.y) )
+    }
+
+    //Member function to print the point.
+    void print() const {
+        cout << "(" << x << "," << y << ")" << endl;
+    }
--- Rectangle1.cpp
+++ Rectangle1.cpp
@@ -2,6 +2,16 @@
 
 // put in the code for the constructors
 
+Rectangle::Rectangle( double w, double h) {
+  width= w;
+  height = h;
+}
+
+Rectangle::Rectangle() {
+  width = 0;
+  height = 0;
+}
+
 void Rectangle::set_width(double w) { width = w; }
 void Rectangle::set_height(double h) { height = h; }
 double Rectangle::get_width() { return width; }
